c23c72683e0d21938ee226cc8640593d3fed8855
noahlvb <noahlvb@gmail.com>
2017-12-27 20:06:16 +0100

    Fixed the showing of the subcommittee report only when finished

--- app/controllers/jc/complaint.js
+++ app/controllers/jc/complaint.js
@@ -19,7 +19,7 @@ router.get('/:id', auth.groups(['jc']), function (req, res) {
                 return res.redirect('/jc/overview')
             }
 
-            jcSubcommittee.find({ complaint: documentComplaint[0].record, done: true }, function (err, documentSubcommittee) {
+            jcSubcommittee.find({ complaint: documentComplaint[0].record }, function (err, documentSubcommittee) {
                 res.render('jc/complaint', {complaint: documentComplaint[0], subcommittee: documentSubcommittee})
             })
         })
