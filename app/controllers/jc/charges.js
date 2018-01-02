692466f6a9175618dcba0f5e5e77a71e353a53e1
noahlvb <noahlvb@gmail.com>
2018-01-02 16:56:16 +0100

    Implemented the creating of charges

--- app/controllers/jc/charges.js
+++ app/controllers/jc/charges.js
@@ -2,7 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const auth = require('./../../middlewares/auth')
+const namesConverter = require('./../../helpers/namesConverter');
 const jcComplaints = require('./../../models/jcComplaint')
+const jcCharges = require('./../../models/jcCharge')
+const laws = require('./../../models/law')
 
 const router = express.Router()
 
@@ -32,6 +35,62 @@ router.get('/proceeding/:id/:action', auth.groups(['jc']), function (req, res) {
                 res.redirect('/jc/complaint/' + req.params.id)
             })
 
+        })
+    } else {
+        req.flash('warning', 'Deze klacht bestaat niet!')
+        return res.redirect('/jc/overview')
+    }
+})
+
+router.post('/:id', auth.groups(['jc']), function (req, res) {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+        jcComplaints.find({_id: req.params.id}, function (err, documentComplaint) {
+            if (err) {
+                return console.log(err);
+            }
+
+            if (documentComplaint.length == 0) {
+                req.flash('warning', 'Deze klacht bestaat niet!')
+                return res.redirect('/jc/overview')
+            }
+
+            for (field in req.body) {
+                if (req.body[field] === '' || req.body[field] === null || req.body[field] === undefined) {
+                    req.flash('warning', 'Vul alle velden in!')
+                    return res.redirect('/jc/complaint/' + req.params.id)
+                }
+            }
+
+            namesConverter.toID(req.body.accused, function (peopleIDs) {
+                if (peopleIDs.length != 1) {
+                    req.flash('warning', 'Geef een naam op!')
+                    return res.redirect('/jc/complaint/' + req.params.id)
+                }
+
+                laws.findOne({ number: req.body.law, enabled: true }, function (err, documentLaw) {
+                    if (!documentLaw) {
+                        req.flash('warning', 'Deze regel bestaat niet!')
+                        return res.redirect('/jc/complaint/' + req.params.id)
+                    }
+
+                    let documentCharge = {
+                        record: documentComplaint.record,
+                        accused: [peopleIDs[0]],
+                        law: req.body.law,
+                        plea: Boolean(req.body.plea)
+                    }
+
+                    new jcCharges(documentCharge).save(function (err) {
+                        if (err) {
+                            req.flash('error', 'Deze aanklacht is al gemaakt')
+                        } else {
+                            req.flash('info', 'Aanklacht aangemaakt')
+                        }
+
+                        res.redirect('/jc/complaint/' + req.params.id)
+                    })
+                })
+            })
         })
     } else {
         req.flash('warning', 'Deze klacht bestaat niet!')
---
+++ app/models/jcCharge.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const idsToNamesFunc = require('./functions/idsToNames')
+
+const jcChargeSchema = mongoose.Schema({
+    record: String,
+    accused: Array,
+    law: String,
+    plea: Boolean
+
+}, {timestamps: { createdAt: 'created_at' } })
+
+jcChargeSchema.index({ record: 1, accused: 1, law: 1 }, { unique: true })
+
+jcChargeSchema.post('find', idsToNamesFunc(['accused']))
+
+const jcChargeModel = mongoose.model('jcCharges', jcChargeSchema)
+
+module.exports = jcChargeModel
