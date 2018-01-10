0fb53946147b1fc9a89a88894043228b35e2643f
noahlvb <noahlvb@gmail.com>
2018-01-10 14:49:18 +0100

    Made the lawsuit page

--- app/controllers/jc/lawsuit.js
+++ app/controllers/jc/lawsuit.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const async = require('async');
+const mongoose = require('mongoose');
 
 const auth = require('./../../middlewares/auth')
 const namesConverter = require('./../../helpers/namesConverter');
@@ -17,6 +18,44 @@ router.get('/', auth.groups(['schoolMeetingVoorzitter']), function (req, res) {
 
         res.render('jc/lawsuit/overview', { lawsuits: document })
     })
+})
+
+router.get('/:id', auth.groups(['schoolMeetingVoorzitter']), function (req, res) {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+        jcLawsuits.find({ _id: req.params.id }, function (err, document) {
+            if (err) {
+                return console.log(err);
+            }
+
+            if (document.length !== 1) {
+                req.flash('warning', 'Deze rechtzaak bestaat niet!')
+                res.redirect('/jc/lawsuit')
+            }
+
+            async.parallel({
+                complaint: function (callback) {
+                    jcComplaints.find({ record: document[0].jcRecord }, function (err, documentComplaint) {
+                        callback(null, documentComplaint[0])
+                    })
+                },
+                charges: function (callback) {
+                    jcCharges.find({ record: document[0].jcRecord }, function (err, documentCharges) {
+                        callback(null, documentCharges)
+                    })
+                },
+                lawsuits: function (callback) {
+                    jcLawsuits.find({ jcRecord: document[0].jcRecord }, function (err, documentLawsuits) {
+                        callback(null, documentLawsuits)
+                    })
+                }
+            }, function (err, result) {
+                res.render('jc/lawsuit/case', { lawsuit: document[0], lawsuits: result.lawsuits, complaint: result.complaint, charges: result.charges, subcommittee: undefined })
+            })
+        })
+    } else {
+        req.flash('warning', 'Deze rechtzaak bestaat niet!')
+        return res.redirect('/jc/overview')
+    }
 })
 
 router.post('/create', auth.groups(['jc']), function (req, res) {
