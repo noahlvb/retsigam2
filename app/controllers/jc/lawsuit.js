2487061e4d3e8397542c94c785e92ce22577dd10
noahlvb <noahlvb@gmail.com>
2018-01-10 15:32:27 +0100

    Implemented lawsuit scheduling

--- app/controllers/jc/lawsuit.js
+++ app/controllers/jc/lawsuit.js
@@ -54,7 +54,36 @@ router.get('/:id', auth.groups(['schoolMeetingVoorzitter']), function (req, res)
         })
     } else {
         req.flash('warning', 'Deze rechtzaak bestaat niet!')
-        return res.redirect('/jc/overview')
+        return res.redirect('/lawsuit/overview')
+    }
+})
+
+router.post('/schedule/:id', auth.groups(['schoolMeetingVoorzitter']), function (req, res) {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+        jcLawsuits.find({ _id: req.params.id }, function (err, document) {
+            if (document.length !== 1) {
+                req.flash('warning', 'Deze rechtzaak bestaat niet!')
+                res.redirect('/jc/lawsuit')
+            }
+
+            if (new Date(req.body.datetime).getTime() < new Date().getTime()) {
+                req.flash('warning', 'Je kan een rechtzaak niet in het verleden plannen!')
+                res.redirect('/jc/lawsuit/' + req.params.id)
+            }
+
+            namesConverter.toID(req.body.jury, function (peopleIDs) {
+                document[0].date = new Date(req.body.datetime)
+                document[0].jury = peopleIDs
+
+                document[0].save(function (err) {
+                    req.flash('info', 'Rechtzaak is ingepland')
+                    res.redirect('/jc/lawsuit')
+                })
+            })
+        })
+    } else {
+        req.flash('warning', 'Deze rechtzaak bestaat niet!')
+        return res.redirect('/lawsuit/overview')
     }
 })
 
