6e0ab5ec5d953291d67bf06ce7927e00d96ae96f
noahlvb <noahlvb@gmail.com>
2018-02-24 14:22:46 +0100

    Made the config fallback to env variables of config file does not exist

--- app/helpers/configLoader.js
+++ app/helpers/configLoader.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 
+let fallback = {
+    "webPort": process.env.PORT,
+    "secret": process.env.secret,
+    "db": process.env.db
+}
+
 module.exports = function () {
     return new Promise(function (resolve, reject) {
         fs.readFile('./config.json', 'utf8', function (err, data) {
-            resolve(JSON.parse(data))
+            if (err) {
+                resolve(fallback)
+            } else {
+                resolve(JSON.parse(data))
+            }
         })
     })
 }
