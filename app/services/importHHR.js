efa5b8d9ae8b7994a26a7358b270e8209d8e00b2
noahlvb <noahlvb@gmail.com>
2018-04-05 16:19:03 +0200

    Implemented Importing of HHR

---
+++ app/services/importHHR.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const laws = require('./../models/law')
+
+module.exports = function (file, callback) {
+    let HHR = []
+
+    var lineReader = readline.createInterface({
+        input: require('fs').createReadStream(file.path, 'utf8')
+    })
+
+    lineReader.on('line', function (line) {
+        let number = line.split(/,(.+)/)[0].trim()
+        if (number.length != 0 && !isNaN(number[0])) {
+            let role = {
+                number: line.split(/,(.+)/)[0],
+                content: line.split(/,(.+)/)[1]
+            }
+
+            HHR.push(role)
+        }
+
+    })
+
+    lineReader.on('close', function () {
+        let rolesToProcess = HHR.map((role) => {
+            return new Promise((resolve) => {
+                laws.new(role, function () {
+                    resolve()
+                })
+            })
+        })
+
+        Promise.all(rolesToProcess).then(() => {
+            callback()
+        })
+    })
+}
